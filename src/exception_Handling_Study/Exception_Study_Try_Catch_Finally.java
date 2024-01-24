7a1c286af72cb5dd67c1542ab2f90a58efcab813
vijayawasthi527 <vijayawasthi527@gmail.com>
2024-01-24 16:26:13 +0530

    [Vijay Awasthi]:New Class is added in exception handling

---
+++ src/exception_Handling_Study/Exception_Study_Try_Catch_Finally.java
@@ -0,0 +1,41 @@
+package exception_Handling_Study;
+
+public class Exception_Study_Try_Catch_Finally {
+
+	public static void main(String[] args) 
+	{
+		int a=0;
+		int b=0;
+		
+		String name="Vijay";
+		
+		
+		
+		try 
+		{
+			//System.out.println(a/b);
+			System.out.println(name.charAt(9));
+		} 
+		catch (StringIndexOutOfBoundsException e)
+		{
+			System.out.println("Cant locate the character out of bound");
+		}
+		
+		catch (IndexOutOfBoundsException e)
+		{
+			System.out.println("This message will show when index is out of bound!!");
+		}
+		
+		catch (ArithmeticException e)
+		{
+			System.out.println("this is real exception");
+		}
+		
+		finally 
+		{
+			System.out.println("Exception has finally tackled");
+		}
+
+	}
+
+}
