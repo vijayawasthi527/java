607c00c9c4673d0543e484ed3af8c46050fb6be2
vijayawasthi527 <vijayawasthi527@gmail.com>
2024-01-25 08:29:40 +0530

    [Vijay Awasthi]:New Class hasbeen Added

---
+++ src/exception_Handling_Study/Try_Finally_study.java
@@ -0,0 +1,17 @@
+package exception_Handling_Study;
+
+public class Try_Finally_study {
+
+	public static void main(String[] args)
+	{
+		try {
+			System.out.println(10/10);
+			
+		} finally 
+		{
+			System.out.println("WelCome All");
+		}
+
+	}
+
+}
